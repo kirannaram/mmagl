b648b563f675fbc97ecfb50d85030f65c40984f1
kiran <kiran.naram@gmail.com>
2018-09-02 16:04:25 +0530

    removed old code and updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,6 @@ import {
   ViewChild,
   ElementRef
 } from "@angular/core";
-import { Router, Routes } from "@angular/router";
-import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: "app-root",
@@ -16,18 +14,10 @@ import { HomeComponent } from "./home/home.component";
 })
 export class AppComponent {
   title = "app";
-  constructor(private router: Router) {}
-  carHmData: string[];
-  yearData: string[];
-  carInfoData: string[];
+  constructor() {}
   //@Output() InternalPageAccess: EventEmitter<any> = new EventEmitter();
-  @ViewChild(HomeComponent) homePath: HomeComponent;
-  @ViewChild("myElem") scrollPosition: ElementRef;
+
   ngOnInit() {
     console.log("test");
-    var pathname = window.location.pathname;
-    if (window.location.pathname.length >= 2) {
-      this.homePath.getcarInfo(pathname);
-    }
   }
 }
